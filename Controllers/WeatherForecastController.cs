8a3baf09fcc0227447a581bb52d7c6a027d84fdf
10563415 <79277291+10563415@users.noreply.github.com>
2021-03-13 19:32:59 +0000

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace DockerAPI.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
